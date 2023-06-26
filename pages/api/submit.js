23135052f6b3e783ea803bc19252bc1d0472c83f
jianmin-chen <jianmin0chen@gmail.com>
2023-06-26 14:08:08 -0400

    Fix submit.js

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -40,24 +40,48 @@ export default async function handler(req, res) {
       Submitted: true
     })
 
-    // Update Application Tracker
-    await trackerAirtable.updateWhere(`{Venue} = "${applicationVenue}"`, {
-      Venue: turnoverRecord.fields['School Name'],
-      'Leader Address': turnoverRecord.fields['Leader Address'].join(','),
-      'Leader(s)': turnoverRecord.fields['Full Name'].join(','),
-      Status: 'turnover',
-      "Leaders' Emails": turnoverRecord.fields['Leaders Emails'].join(','),
-      Location: turnoverRecord.fields['School Address'],
-      'Leader Phone': turnoverRecord.fields['Leader Phone'].join(','),
-      Applied: turnoverRecord.fields['Turnover Date'],
-      'Turnover Database': turnoverRecord.id,
-      Notes: 'Leader applied for turnover',
-      'Date Responded': dayjs(new Date().toString()).format('MM/DD/YYYY')
-    })
+    if (
+      !(
+        await trackerAirtable.read({
+          filterByFormula: `{Venue} = "${applicationVenue}"`,
+          maxRecords: 1
+        })
+      ).length
+    ) {
+      // Club doesn't exist in toolbox yet
+      await trackerAirtable.create({
+        Venue: turnoverRecord.fields['School Name'],
+        'Leader Address': turnoverRecord.fields['Leader Address'].join(','),
+        'Leader(s)': turnoverRecord.fields['Full Name'].join(','),
+        Status: 'turnover',
+        "Leaders' Emails": turnoverRecord.fields['Leaders Emails'].join(','),
+        Location: turnoverRecord.fields['School Address'],
+        'Leader Phone': turnoverRecord.fields['Leader Phone'].join(','),
+        Applied: turnoverRecord.fields['Turnover Date'],
+        'Turnover Database': turnoverRecord.id,
+        Notes: 'Leader applied for turnover',
+        'Date Responded': dayjs(new Date().toString()).format('MM/DD/YYYY')
+      })
+    } else {
+      // Update Application Tracker
+      await trackerAirtable.updateWhere(`{Venue} = "${applicationVenue}"`, {
+        Venue: turnoverRecord.fields['School Name'],
+        'Leader Address': turnoverRecord.fields['Leader Address'].join(','),
+        'Leader(s)': turnoverRecord.fields['Full Name'].join(','),
+        Status: 'turnover',
+        "Leaders' Emails": turnoverRecord.fields['Leaders Emails'].join(','),
+        Location: turnoverRecord.fields['School Address'],
+        'Leader Phone': turnoverRecord.fields['Leader Phone'].join(','),
+        Applied: turnoverRecord.fields['Turnover Date'],
+        'Turnover Database': turnoverRecord.id,
+        Notes: 'Leader applied for turnover',
+        'Date Responded': dayjs(new Date().toString()).format('MM/DD/YYYY')
+      })
 
-    // Update Clubs Dashboard
-    // Note: Actually, this involves removing the old venue from the club dashboard
-    await clubsAirtable.deleteWhere(`{Venue} = "${applicationVenue}"`)
+      // Update Clubs Dashboard
+      // Note: Actually, this involves removing the old venue from the club dashboard
+      await clubsAirtable.deleteWhere(`{Venue} = "${applicationVenue}"`)
+    }
 
     return res.status(200).json({ success: true, id: application.id })
   } catch (error) {
